901220f498f18dbea8d69c6fac2976ff884dde26
Anish Raj <a.rajv11@gmail.com>
2022-08-11 22:56:53 -0400

    first commit

---
+++ src/components/TopBar.js
@@ -0,0 +1,24 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
+
+export default function TopBar() {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg" >
+                <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="#home">
+                    <img src="https://www.instacart.com/company/wp-content/themes/instacartcorporate/theme-assets/images/ShorthandLogo.svg" style={{width: "10%"}} alt="instacart"/>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto"> 
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#LogIn">Log In</Nav.Link>
+                        <Button variant="success">Sign Up</Button>{' '}
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    )
+}
